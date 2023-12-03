4a944a446f0bebc1f78be34b92d1141a9c2ac864
jgferrell <ringtail.jack+git@gmail.com>
2023-12-02 21:20:21 -0500

    Unicode error fix

--- weather/downloader.py
+++ weather/downloader.py
@@ -3,5 +3,5 @@ import requests
 def download_resource(remote, local):
     r = requests.get(remote)
     r.raise_for_status()
-    with open(local, 'w') as f:
+    with open(local, 'w', encoding='utf-8') as f:
         f.write(r.text)
